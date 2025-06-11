a74d21a00ae81c44ba20b005cf618518ce28c6e4
ysnd <yusnadi.aunur@gmail.com>
2025-06-11 15:02:41 +0700

    blink in framework Arduino for Generic ESP32 Board

---
+++ Week1/Day1/esp32-gpio-testb2/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#define LED 2
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(LED, HIGH);
+  Serial.println("ON");
+  delay(1000);
+  digitalWrite(LED, LOW);
+  Serial.println("OFF");
+  delay(1000);
+}
