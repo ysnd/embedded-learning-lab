6f8ede1518039be66668f05b68cf51a8c6aea970
ysnd <yusnadi.aunur@gmail.com>
2025-08-27 22:38:04 +0700

    add example use component bmp280 barometric pressure temperature sensor

---
+++ components/example/bmp280_sensor/main/bmp280_sensor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/i2c.h"
+#include "BMP280.h"
+
+void app_main(void)
+{
+    bmp280_conf_t bmp = {
+        .i2c_port = I2C_NUM_0,
+        .sda_pin = 21,
+        .scl_pin = 22,
+        .freq_hz = 100000
+    };
+    bmp280_init(&bmp);
+
+    while (1) {
+        bmp280_update_raw();
+        double temp = bmp280_get_temp();
+        double press = bmp280_get_pressure();
+        double alt = bmp280_get_altitude();
+
+        printf("Temp : %.2f C | Pressure : %.2f hPa | Altitude : %.2f m\n", temp, press/100.0, alt);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
