993ec5d7a2258e7a4ab2770421c658f38bb3fa3d
ysnd <yusnadi.aunur@gmail.com>
2025-09-16 20:31:28 +0700

    init control led and read dht11 with bluetooth low energy

---
+++ Bluetooth/BLE_led_dht/main/BLE_led_dht.c
@@ -0,0 +1,348 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include "esp_err.h"
+#include "nvs_flash.h"
+#include "esp_log.h"
+#include "driver/gpio.h"
+#include "esp_bt.h"
+#include "esp_bt_main.h"
+#include "esp_gap_ble_api.h"
+#include "esp_gatts_api.h"
+#include "esp_gatt_defs.h"
+#include "esp_gatt_common_api.h"
+#include "dht11.h"
+
+#define LED 2
+#define DHT 17
+
+//uuid
+#define SERVICE_UUID 0x00FF
+#define CHAR_UUID_LED 0xFF01
+#define CHAR_UUID_DHT 0xFF02
+
+static const esp_ble_adv_params_t adv_params = {
+    .adv_int_min = 0x20,
+    .adv_int_max = 0x40,
+    .adv_type = ADV_TYPE_IND,
+    .own_addr_type = BLE_ADDR_TYPE_PUBLIC,
+    .channel_map = ADV_CHNL_ALL,
+    .adv_filter_policy = ADV_FILTER_ALLOW_SCAN_ANY_CON_ANY
+};
+
+static uint16_t gl_service_handle = 0;
+static esp_gatt_if_t gl_gatts_if = 0;
+static uint16_t gl_conn_id = 0;
+static bool gl_connected = false;
+
+static uint16_t gl_led_value_handle = 0;
+static uint16_t gl_led_cccd_handle = 0;
+static bool gl_led_notify_en = false;
+
+static uint16_t gl_dht_value_handle = 0;
+static uint16_t gl_dht_cccd_handle = 0;
+static bool gl_dht_notify_en = false;
+
+static bool led_char_added = false;
+static bool led_cccd_added = false;
+
+static void make_uuid16(esp_bt_uuid_t *u, uint16_t u16) {
+    u->len = ESP_UUID_LEN_16;
+    u->uuid.uuid16 = u16;
+}
+
+static uint8_t adv_service_uuid128[16] = {  
+    0xfb, 0x34, 0x9b, 0x5f, 0x80, 0x00, 0x00, 0x80,
+    0x00, 0x10, 0x00, 0x00, (SERVICE_UUID & 0xFF), (SERVICE_UUID >> 8), 0x00, 0x00  
+};
+
+//gap callback
+static void gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param) {
+    switch (event) {
+    case ESP_GAP_BLE_ADV_DATA_SET_COMPLETE_EVT:
+        printf("adv data set complete -> start adv\n");
+        esp_ble_gap_start_advertising(&adv_params);
+        break;
+    default:
+        break;
+    }
+}
+
+static void send_notify_uint8(esp_gatt_if_t gatts_if, uint16_t conn_id, uint16_t value_handle, uint8_t val) {
+    if (conn_id == 0) return;
+    esp_err_t err = esp_ble_gatts_send_indicate(gatts_if, conn_id, value_handle, sizeof(val), &val, false);
+    printf("send indicate(handle=%d) - %s\n", value_handle, esp_err_to_name(err));
+}
+
+static void dht_task(void *arg) {
+    dht11_data_t d;
+    while (1) {
+        if (gl_connected && gl_dht_notify_en && gl_dht_value_handle !=0) {
+            if (dht11_read(&d)) {
+                // pack temperature & humidity as two int16 (x10) little endian
+                int16_t t10 = (int16_t)(d.temperature * 10.0);
+                int16_t h10 = (int16_t)(d.humidity * 10.0);
+                uint8_t payload[4];
+                payload[0] = t10 & 0xFF;
+                payload[1] = (t10 >> 8) & 0xFF;
+                payload[2] = h10 & 0xFF;
+                payload[3] = (h10 >> 8) & 0xFF;
+                esp_err_t e = esp_ble_gatts_send_indicate(gl_gatts_if, gl_conn_id, gl_dht_value_handle, sizeof(payload), payload, false);
+                printf("DHT notify T = %.1f H=%.1f -> send indicate : %s\n", d.temperature, d.humidity, esp_err_to_name(e));
+            } else {
+                printf("DHT Read failed\n");
+            }
+        }
+        vTaskDelay(pdMS_TO_TICKS(2000));
+    }
+}
+
+// gatts event handler
+static void gatts_cb(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if, esp_ble_gatts_cb_param_t *param) {
+    esp_err_t err;
+    switch (event) {
+    case ESP_GATTS_REG_EVT:
+        printf("GATTS_REG_EVT, appp_id %d\n", param->reg.app_id);
+        //set device name
+        esp_ble_gap_set_device_name("ESP32_LED_BLE");
+        //minimal adv data: include name +service uuid16
+        {
+            
+            esp_ble_adv_data_t adv_data = {
+                .set_scan_rsp = false,
+                .include_name = true,
+                .include_txpower = true,
+                .service_uuid_len = sizeof(adv_service_uuid128),
+                .p_service_uuid = adv_service_uuid128,
+                .flag = ESP_BLE_ADV_FLAG_GEN_DISC | ESP_BLE_ADV_FLAG_BREDR_NOT_SPT,
+            };
+            esp_ble_gap_config_adv_data(&adv_data);// triggers ADV_DATA_SET_COMPLETE event
+        }
+        //create service
+        {
+            esp_gatt_srvc_id_t srvc_id;
+            memset(&srvc_id, 0, sizeof(srvc_id));
+            srvc_id.is_primary = true;
+            srvc_id.id.inst_id = 0;
+            make_uuid16(&srvc_id.id.uuid, SERVICE_UUID);
+
+            err = esp_ble_gatts_create_service(gatts_if, &srvc_id, 8);
+            if (err != ESP_OK) {
+                printf("create service failed : %s\n", esp_err_to_name(err));
+            }
+        }
+        break;
+    
+    case ESP_GATTS_CREATE_EVT:
+        printf("CREATE_EVT, status %d, service_handle %d\n", param->create.status, param->create.service_handle);
+        gl_service_handle = param->create.service_handle;
+
+        //start service
+        esp_ble_gatts_start_service(gl_service_handle);
+
+        //add caracteristic (read/write/notify)
+        {
+            esp_bt_uuid_t char_uuid;
+            make_uuid16(&char_uuid, CHAR_UUID_LED);
+            esp_attr_value_t char_val = {
+                .attr_max_len = 1,
+                .attr_len = 1,
+                .attr_value = (uint8_t[]){0x00}
+            };
+            err = esp_ble_gatts_add_char(gl_service_handle, &char_uuid, ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE, ESP_GATT_CHAR_PROP_BIT_READ | ESP_GATT_CHAR_PROP_BIT_WRITE | ESP_GATT_CHAR_PROP_BIT_NOTIFY, &char_val, NULL);
+            printf("add char LED -> %s\n", esp_err_to_name(err));
+        }   
+        break;
+
+    case ESP_GATTS_ADD_CHAR_EVT:
+        printf("ADD_CHAR_EVT, attr_handle %d uuid=0x%04x\n", param->add_char.attr_handle, (param->add_char.char_uuid.len == ESP_UUID_LEN_16) ? param->add_char.char_uuid.uuid.uuid16 : 0);
+        if (param->add_char.char_uuid.len == ESP_UUID_LEN_16) {
+            uint16_t u = param->add_char.char_uuid.uuid.uuid16;
+            if (u == CHAR_UUID_LED) {
+                gl_led_value_handle = param->add_char.attr_handle;
+                led_char_added = true;
+                printf("LED Value handle = %d\n", gl_led_value_handle);
+            } else if (u == CHAR_UUID_DHT) {
+                gl_dht_value_handle = param->add_char.attr_handle;
+                printf("DHT Value handle = %d\n", gl_dht_value_handle);
+            } else {
+                printf("unknown char_uudi\n");
+            }
+        }
+        //add cccd descriptor for notify
+        {
+            esp_bt_uuid_t descr_uuid;
+            make_uuid16(&descr_uuid, ESP_GATT_UUID_CHAR_CLIENT_CONFIG); //0x2902    
+            esp_attr_value_t descr_val = {
+                .attr_max_len = 2,
+                .attr_len = 2,
+                .attr_value = (uint8_t[]){0x00, 0x00}
+            };
+            err = esp_ble_gatts_add_char_descr(gl_service_handle, &descr_uuid, ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE, &descr_val, NULL);
+            if (err != ESP_OK) {
+                printf("add_descr failed : %s\n", esp_err_to_name(err));
+            }
+        }
+        break;
+
+    case ESP_GATTS_ADD_CHAR_DESCR_EVT:
+        printf("ADD_DESCR_EVT, hande %d\n", param->add_char_descr.attr_handle);
+        if (led_char_added && !led_cccd_added) {
+            gl_led_cccd_handle = param->add_char_descr.attr_handle;
+            led_cccd_added = true;
+            printf("mapped led_cccd_handle = %d\n", gl_led_cccd_handle);
+            //dht characteristic
+            esp_bt_uuid_t char_uuid;
+            make_uuid16(&char_uuid, CHAR_UUID_DHT);
+            esp_attr_value_t char_val = {
+                .attr_max_len = 4,
+                .attr_len = 4,
+                .attr_value = (uint8_t[]){0x00,0x00,0x00,0x00}
+            };
+            err = esp_ble_gatts_add_char(gl_service_handle, &char_uuid, ESP_GATT_PERM_READ, ESP_GATT_CHAR_PROP_BIT_READ | ESP_GATT_CHAR_PROP_BIT_NOTIFY, &char_val, NULL);
+            printf("add char DHT -> %s",esp_err_to_name(err));
+        } else if (gl_dht_value_handle != 0 && gl_dht_cccd_handle ==0) {
+            gl_dht_cccd_handle = param->add_char_descr.attr_handle;
+            printf("mapped dht_cccd_handle = %d\n", gl_dht_cccd_handle);
+        } else {
+            printf("descriptor mapping ambigous / ignored\n");
+
+        }
+        break;
+
+    case ESP_GATTS_START_EVT:
+        printf("SERVICE START_EVT\n");
+        break;
+
+    case ESP_GATTS_CONNECT_EVT:
+        printf("CONNECT_EVT, conn_id %d\n", param->connect.conn_id);
+        gl_gatts_if = gatts_if;
+        gl_conn_id = param->connect.conn_id;
+        gl_connected = true;
+        gl_led_notify_en = false; //reset notify new conection
+        gl_dht_notify_en = false;
+        break;
+
+    case ESP_GATTS_DISCONNECT_EVT:
+        printf("Disconect reason = 0x%04x\n", param->disconnect.reason);
+        gl_connected = false;
+        gl_dht_notify_en = false;
+        gl_led_notify_en = false;
+        gl_conn_id = 0;
+        //restart adv   
+        esp_ble_gap_start_advertising(&adv_params);
+        break;
+
+    case ESP_GATTS_WRITE_EVT:
+        printf("WRITE_EVT handle=%d, len=%d need_rsp=%d\n", param->write.handle, param->write.len, param->write.need_rsp);
+        //send write response first
+        if (param->write.need_rsp) {
+            esp_gatt_rsp_t rsp;
+            memset(&rsp, 0, sizeof(rsp));
+            rsp.attr_value.handle = param->write.handle;
+            rsp.attr_value.len = (param-> write.len <= sizeof(rsp.attr_value.value)) ? param->write.len : sizeof(rsp.attr_value.value);
+            if (param->write.len > 0) memcpy(rsp.attr_value.value, param->write.value, rsp.attr_value.len);
+            esp_ble_gatts_send_response(gatts_if, param->write.conn_id, param->write.trans_id, ESP_GATT_OK, &rsp);
+            printf("Sent write response\n");            
+        }
+
+        if (param->write.handle == gl_led_cccd_handle) {
+            //data written to char
+            if (param->write.len >= 2) {
+                uint8_t cfg = param->write.value[0] | (param->write.value[1] << 8);
+                gl_led_notify_en = (cfg & 0x0001) ? true : false;
+                printf("LED cccd set to 0x%04x -> notify=%d\n", cfg, gl_led_notify_en);
+            }
+            break;
+        } else if (param->write.handle == gl_dht_cccd_handle) {
+            if (param->write.len >= 2) {
+                uint16_t cfg = param->write.value[0] | (param->write.value[1] << 8);
+                gl_dht_notify_en = (cfg & 0x0001) ? true : false;
+                printf("DHT CCCD set to 0x%04x -> notify=%d\n", cfg, gl_dht_notify_en);
+            }
+            break;
+        }
+        if (param->write.handle == gl_led_value_handle) {
+            if (param->write.len >= 1) {
+                uint8_t v = param->write.value[0];
+                gpio_set_level(LED, v ? 1 : 0);
+                printf("LED set to %d\n", v);
+                // send notify to client if enabled
+                if (gl_connected && gl_led_notify_en && gl_led_value_handle != 0) {
+                    send_notify_uint8(gl_gatts_if, gl_conn_id, gl_led_value_handle, v);
+                }
+            }
+            break;
+        }
+        break;
+
+    case ESP_GATTS_READ_EVT:
+        printf("READ_EVT handle = %d\n", param->read.handle);
+        if (param->read.handle == gl_led_value_handle) {
+            //return current led STate  
+            esp_gatt_rsp_t rsp;
+            memset(&rsp, 0, sizeof(esp_gatt_rsp_t));
+            rsp.attr_value.handle = param->read.handle;
+            rsp.attr_value.len = 1;
+            rsp.attr_value.value[0] = gpio_get_level(LED);
+            esp_ble_gatts_send_response(gatts_if, param->read.conn_id, param->read.trans_id, ESP_GATT_OK, &rsp);
+            printf("Replied LED read = %d", rsp.attr_value.value[0]);
+        } else if (param->read.handle == gl_dht_value_handle) {
+            // read DHT & respond with 4 bytes (temp*10 little endian, hum*10 little endian)
+            dht11_data_t d;
+            esp_gatt_rsp_t rsp;
+            memset(&rsp, 0, sizeof(rsp));
+            rsp.attr_value.handle = param->read.handle;
+            rsp.attr_value.len = 4;
+            if (dht11_read(&d)) {
+                int16_t t10 = (int16_t)(d.temperature * 10.0);
+                int16_t h10 = (int16_t)(d.humidity * 10.0);
+                rsp.attr_value.value[0] = t10 & 0xFF;
+                rsp.attr_value.value[1] = (t10 >> 8) & 0xFF;
+                rsp.attr_value.value[2] = h10 & 0xFF;
+                rsp.attr_value.value[3] = (h10 >> 8) & 0xFF;
+                printf("DHT read for READ_EVT: T=%.1f H=%.1f\n", d.temperature, d.humidity);
+            } else {
+                rsp.attr_value.value[0] = rsp.attr_value.value[1] = rsp.attr_value.value[2] = rsp.attr_value.value[3] = 0xFF;
+                printf("DHT read failed for READ_EVT\n");
+            }
+            esp_ble_gatts_send_response(gatts_if, param->read.conn_id, param->read.trans_id, ESP_GATT_OK, &rsp);
+        }
+        break;
+
+    default:
+        break;
+    }
+}
+
+void app_main(void) {
+    esp_err_t ret;
+
+    ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        nvs_flash_erase();
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    gpio_reset_pin(LED);
+    gpio_set_direction(LED, GPIO_MODE_OUTPUT);
+    gpio_set_level(LED, 0);
+
+    dht11_init(DHT);
+
+    ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT));
+    esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_bt_controller_init(&bt_cfg));
+    ESP_ERROR_CHECK(esp_bt_controller_enable(ESP_BT_MODE_BLE));
+
+    ESP_ERROR_CHECK(esp_bluedroid_init());
+    ESP_ERROR_CHECK(esp_bluedroid_enable());
+
+    ESP_ERROR_CHECK(esp_ble_gap_register_callback(gap_cb));
+    ESP_ERROR_CHECK(esp_ble_gatts_register_callback(gatts_cb));
+    ESP_ERROR_CHECK(esp_ble_gatts_app_register(0));
+    xTaskCreate(dht_task, "dht_task", 4096, NULL, 5, NULL);
+    printf("BLE LED+DHT GATT Server Started...\n");
+}
+
